8ca5b4850995c39a68bcb43ad3584956de746b24
pragathik25 <pkshetty4446@gmail.com>
2023-07-12 12:27:43 +0530

    test module

--- src/Form/AdminConfig.php
+++ src/Form/AdminConfig.php
@@ -6,7 +6,7 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Class AdminConfig.
+ * Implementing config form.
  */
 class AdminConfig extends ConfigFormBase {
 
